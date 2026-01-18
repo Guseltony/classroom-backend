103a6f59f963e68751f6c2bcc1b00e0ee84e4a88
Akanji Anthony Guseltony <devguselt@gmail.com>
2026-01-18 15:13:11 +0100

    implement get all subjects routes and logic

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 import express from "express";
+import subjectsRouter from "./routes/subjects";
+import cors from 'cors'
 
 const app = express();
+const port = 8000;
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}))
 
 app.use(express.json());
+
+app.use('/api/subjects', subjectsRouter )
 
 app.get("/", (req, res) => {
   res.send("Hello, wecome to the classroom API");
 });
 
-const port = 8000;
 
 app.listen(port, () => {
-  console.log(`server listening to http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
