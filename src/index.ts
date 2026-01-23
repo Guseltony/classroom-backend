6908494674aaf5cb2d1107d99e91b0361d8012a8
Akanji Anthony Guseltony <devguselt@gmail.com>
2026-01-23 03:00:47 +0100

    implement better auth

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,25 @@ import express from "express";
 import subjectsRouter from "./routes/subjects";
 import cors from 'cors'
 import securityMiddleware from "./middleware/security";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app = express();
 const port = 8000;
 
 if (!process.env.FRONTEND_URL) {
-  throw new Error("FRONTEND_URL is not set in .env file")
+  throw new Error("FRONTEND_URL is not set in .env file");
 }
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json());
 
